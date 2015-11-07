d14104b2d85cd90e0d070b986e0ffa11c3e8c508
Josh Bebbington <josh@focalwebsites.com>
2015-11-07 16:57:40 +0000

    Feature test passing: When there are no results, a flash message is displayed

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,7 @@
 class SearchController < ApplicationController
   def index
+    if params[:postcode]
+      flash[:no_results] = 'There are no results, please search again'
+    end
   end
 end
--- spec/features/search_feature_spec.rb
+++ spec/features/search_feature_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
 feature 'Searching for a coffee shop' do
+  before :each do
+    visit '/'
+  end
+  
   context 'When on the homepage' do
     it 'Displays a post code input' do
-      visit '/'
-
       expect(page).to have_field('postcode')
+    end
+  end
+
+  context 'When there are no results for a postcode' do
+    it 'A notice is displayed' do
+      fill_in('postcode', with: 'B4D 1NPUT')
+      click_button('Search')
+
+      expect(page).to have_content('There are no results, please search again')
     end
   end
 end
